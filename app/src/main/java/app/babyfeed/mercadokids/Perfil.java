f6c53a16f5e0b15fbf0a1bbcb4473a6352254050
RHNRaul <130188275+RHNRaul@users.noreply.github.com>
2023-05-01 17:48:12 -0600

    Funcionalidad de hijo
    Se agrego el que el hijo pueda iniciar sesion

--- app/src/main/java/app/babyfeed/mercadokids/Perfil.java
+++ app/src/main/java/app/babyfeed/mercadokids/Perfil.java
@@ -2,6 +2,7 @@ package app.babyfeed.mercadokids;
 
 import DAO.PadreDAO;
 import android.content.Intent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +12,7 @@ import java.io.Serializable;
 
 public class Perfil extends AppCompatActivity implements Serializable {
 private TextView et1;
-
+private PadreDAO padre;
 
     // Obtener el objeto PadreDAO del intent
     @Override
@@ -19,11 +20,20 @@ private TextView et1;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
         et1 = findViewById(R.id.NombrePerfil);
-        PadreDAO padre = (PadreDAO) getIntent().getExtras().getSerializable("padre");
+        padre = (PadreDAO) getIntent().getExtras().getSerializable("padre");
         et1.setText("Nombre "+padre.getNom()+" "+padre.getApellidom()+" "+padre.getApellidop()+"\n Codigo Paternal: "+padre.getCodigoarental());
 
     }
+   public void CrearHijo(View view){
+        Intent ventana  = new Intent(this, RegistroHijo.class);
+        ventana.putExtra("codigo",padre.getCodigoarental());
+        startActivity(ventana);
+   }
 
+
+   public void CerrarSesion(View view){
+        finish();
+   }
 
 
 }
